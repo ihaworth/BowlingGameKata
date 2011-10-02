2229702398cbda063bf6b15049aaef2e87896dc6
ian <ihaworth@yahoo.com>
2011-10-02 16:03:00 +0100

    Add test for a game consisting of single pins being knocked down with every roll.

--- tests/bowling/BowlingGameTest.java
+++ tests/bowling/BowlingGameTest.java
@@ -16,4 +16,15 @@ public class BowlingGameTest
         
         assertEquals(0, g.score());
     }
+    
+    @Test
+    public void testAllOnes()
+    {
+        Game g = new Game();
+        
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        
+        assertEquals(20, g.score());
+    }
 }
