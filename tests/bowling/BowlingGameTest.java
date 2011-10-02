1d9ca54d6a901461a9affc149496a431f33bf7cc
ian <ihaworth@yahoo.com>
2011-10-02 16:08:11 +0100

    Extract the Game as a field and initialize it in setup(), and pull out the rollMany() method.

--- tests/bowling/BowlingGameTest.java
+++ tests/bowling/BowlingGameTest.java
@@ -1,30 +1,37 @@
 package bowling;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest
 {
+    private Game g;
+
+    @Before
+    public void setup()
+    {
+        g = new Game();
+    }
+
+    private void rollMany4(int n, int pins)
+    {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
     @Test
     public void testGutterGame()
     {
-        Game g = new Game();
-        
-        for (int i = 0; i < 20; i++)
-            g.roll(0);
-        
+        rollMany4(20, 0);
         assertEquals(0, g.score());
     }
     
     @Test
     public void testAllOnes()
     {
-        Game g = new Game();
-        
-        for (int i = 0; i < 20; i++)
-            g.roll(1);
-        
+        rollMany4(20, 1);
         assertEquals(20, g.score());
     }
 }
