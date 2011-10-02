a3c742ec1dc8050b926557b5e8d968d7017ce36f
ian <ihaworth@yahoo.com>
2011-10-02 16:04:07 +0100

    Simple implementation to get the test to pass.

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -2,12 +2,15 @@ package bowling;
 
 public class Game
 {
+    private int score;
+
     public void roll(int pins)
     {
+        score += pins;
     }
 
     public int score()
     {
-        return 0;
+        return score;
     }
 }
