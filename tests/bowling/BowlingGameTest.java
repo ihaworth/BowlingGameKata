54bbeeaabf61c4e8b0f118ba2926d08ccc0d3845
ian <ihaworth@yahoo.com>
2011-10-02 17:59:50 +0100

    A little refactoring to make the test more readable.

--- tests/bowling/BowlingGameTest.java
+++ tests/bowling/BowlingGameTest.java
@@ -47,11 +47,16 @@ public class BowlingGameTest
     @Test
     public void testOneStrike()
     {
-        g.roll(10);
+        rollStrike();
         g.roll(3);
         g.roll(4);
         rollMany(16, 0);
         assertEquals(24, g.score());
+    }
+
+    private void rollStrike()
+    {
+        g.roll(10);
     }
 
     private void rollSpare(int pins)
