162f8fb06e5382b6a1ec64c0722fdaed4881a20f
ian <ihaworth@yahoo.com>
2011-10-02 15:48:58 +0100

    Initial failing test.

---
+++ tests/bowling/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest
+{
+    @Test
+    public void testGutterGame()
+    {
+        fail("Not yet implemented");
+    }
+}
