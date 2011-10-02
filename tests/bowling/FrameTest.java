1c82518460d60b0c3629acc7e81a0421c4624dcd
ian <ihaworth@yahoo.com>
2011-10-02 16:19:32 +0100

    Start developing the Frame class test-first by adding the FrameTest with an failing implementation of testGutterFrame().

---
+++ tests/bowling/FrameTest.java
@@ -0,0 +1,14 @@
+package bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FrameTest
+{
+    @Test
+    public void testGutterFrame()
+    {
+        fail("Not yet implemented");
+    }
+}
