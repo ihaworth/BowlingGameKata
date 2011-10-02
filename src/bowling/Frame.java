1cc6b59af80fe9e400fb68a8e30cf62a918ca1f0
ian <ihaworth@yahoo.com>
2011-10-02 18:23:08 +0100

    Allow the LastFrame to hold 3 rolls.

--- src/bowling/Frame.java
+++ src/bowling/Frame.java
@@ -4,10 +4,20 @@ public class Frame
 {
     private static final int MAX_ROLLS = 2;
 
-    private int[] rolls = new int[MAX_ROLLS];
+    protected int[] rolls;
     protected int currentRoll = 0;
 
     private Frame nextFrame;
+
+    public Frame()
+    {
+        init();
+    }
+
+    protected void init()
+    {
+        rolls = new int[MAX_ROLLS];
+    }
 
     public void setNextFrame(Frame nextFrame)
     {
--- src/bowling/LastFrame.java
+++ src/bowling/LastFrame.java
@@ -2,6 +2,14 @@ package bowling;
 
 public class LastFrame extends Frame
 {
+    private static final int MAX_ROLLS = 3;
+
+    @Override
+    protected void init()
+    {
+        rolls = new int[MAX_ROLLS];
+    }
+
     @Override
     public boolean isOver()
     {
