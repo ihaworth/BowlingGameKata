d6ed498f5ec2fe86c73d0c0c9a261d8c1a4af70e
ian <ihaworth@yahoo.com>
2011-10-02 17:18:45 +0100

    Implementation will require an isStrike() method.

--- src/bowling/Frame.java
+++ src/bowling/Frame.java
@@ -45,6 +45,6 @@ public class Frame
 
     public boolean isSpare()
     {
-        return firstRoll() + secondRoll() == 10;
+        return !isStrike() && firstRoll() + secondRoll() == 10;
     }
 }
